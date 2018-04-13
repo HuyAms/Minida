6b000ef06d02c4aad2899853b428b3882a1fb611
anniluo <anniluo@metropolia.fi>
2018-04-13 14:40:28 +0300

    adding ds store to ignore

--- Project_Ios/Enums/StoryBoardID.swift
+++ Project_Ios/Enums/StoryBoardID.swift
@@ -12,6 +12,8 @@ enum AppStoryBoard: String{
     case tabBarVC = "tabBarScreen"
     case authVC = "authScreen"
     case onBoardingVC = "onboardingScreen"
+    case profileVC = "profileScreen"
+    case mapVC = "mapScreen"
     
     var identifier: String {
         return self.rawValue
